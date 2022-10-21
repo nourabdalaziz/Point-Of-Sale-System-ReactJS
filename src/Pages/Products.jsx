9f201d867b8ee0206b8dc365075e4e50c148ed51
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-21 18:44:27 +0300

    display fetched data with table component

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,13 +1,36 @@
 import useFetch from "../CustomHooks/useFetch.jsx";
+import { useState } from "react";
+import FilterableTable from "../Components/FilterableTable.jsx";
 
 const Products = () => {
-  const [products, isLoading] = useFetch("http://localhost:3000/products");
+  const [products, isLoading] = useFetch("http://localhost:5000/products");
+  const [searchedValue, setSearchedValue] = useState("");
+  const HEADERS = ["code", "name", "category", "image"];
 
-  console.log(products);
+  const editProduct = (id) => {
+    console.log(id, ".. hello from product editing function");
+  };
+
   return (
     <div>
-      <h1>Hello from Products</h1>
-      {isLoading ? <h2>Loading...</h2> : <h2>Finished fetching</h2>}{" "}
+      {isLoading ? (
+        <h2>Loading...</h2>
+      ) : (
+        products && (
+          <>
+            <input
+              type="search"
+              onChange={(e) => setSearchedValue(e.target.value)}
+            />
+            <FilterableTable
+              headers={HEADERS}
+              dataInTable={products}
+              searchedValue={searchedValue}
+              editProduct={editProduct}
+            />
+          </>
+        )
+      )}
     </div>
   );
 };
