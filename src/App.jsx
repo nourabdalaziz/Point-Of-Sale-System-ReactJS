ee88e01b36f428af5a9d03e416e7183d01328b5c
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-21 17:10:39 +0300

    implement routes to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Categories from "./Pages/Categories.jsx";
+import POS from "./Pages/POS.jsx";
+import Products from "./Pages/Products.jsx";
+import Navbar from "./Components/Navbar.jsx";
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<POS />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/pos" element={<POS />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
