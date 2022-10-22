e70ee8f023e785405f2ccd6d42f13be493c75396
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-22 21:03:52 +0300

    display loading spinner while fetching data

---
+++ src/Components/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import "./loadingSpinner.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="main-container-spinner">
+      <div className="spinner-container">
+        <div className="loading-spinner"></div>
+      </div>
+    </div>
+  );
+};
+export default LoadingSpinner;
