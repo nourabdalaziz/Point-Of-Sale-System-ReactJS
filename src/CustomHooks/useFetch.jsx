3af8e63fd5210243ba2fd1ce2013ec9c6412839d
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-22 16:55:41 +0300

    use cleanup fun to handle: can't perform state update on unmounted component

--- src/CustomHooks/useFetch.jsx
+++ src/CustomHooks/useFetch.jsx
@@ -5,8 +5,10 @@ const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
+      fetch(url, { signal: abortCont.signal })
         .then((res) => {
           if (!res.ok) {
             throw Error("could not fetch the data for that resource");
@@ -18,9 +20,14 @@ const useFetch = (url) => {
           setData(data);
         })
         .catch((err) => {
-          console.log("Failed to fetch data: ", err);
+          if (err.name === "AbortError") {
+            console.log("fetch aborted");
+          } else {
+            console.log(err.message);
+          }
         });
     }, 1000);
+    return () => abortCont.abort();
   }, [url]);
 
   return [data, isLoading];
