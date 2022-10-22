39f7c8f6dc4386028888dc6ca48ad27375cb5068
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-22 17:58:25 +0300

    display pagination at the end of table

--- src/Components/FilterableTable.jsx
+++ src/Components/FilterableTable.jsx
@@ -1,4 +1,6 @@
 import "./filterableTable.css";
+import { useState } from "react";
+import Pagination from "./Pagination.jsx";
 
 const FilterableTable = ({
   headers,
@@ -6,6 +8,19 @@ const FilterableTable = ({
   searchedValue,
   editProduct,
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 3;
+  const indexOfLastPage = currentPage * itemsPerPage;
+  const indexOfFirstPage = indexOfLastPage - itemsPerPage;
+  const currentItems = dataInTable.slice(indexOfFirstPage, indexOfLastPage);
+
+  const pageNumbers = [];
+  const totalItems = dataInTable.length;
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
   function isURL(str) {
     var pattern = new RegExp(
       "^(https?:\\/\\/)?" + // protocol
@@ -24,55 +39,66 @@ const FilterableTable = ({
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          {" "}
-          {headers.map((header) => {
-            return <th key={Math.random()}>{capetalizeFirstLetter(header)}</th>;
-          })}
-          <th key={Math.random()}>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataInTable
-          .filter((row) => {
-            let res = false;
-            let vals = Object.values(row);
-            if (isURL(vals[length - 1])) vals.pop();
-            for (const val of vals) {
-              res = val
-                .toString()
-                .toLowerCase()
-                .includes(searchedValue.toString().toLowerCase());
-              if (res) break;
-            }
-            return res;
-          })
-          .map((item) => {
-            return (
-              <tr key={item.id}>
-                {headers.map((header) => {
-                  return header === "image" ? (
+    <div className="main-container">
+      <div className="table-component">
+        <table>
+          <thead>
+            <tr>
+              {" "}
+              {headers.map((header) => {
+                return (
+                  <th key={Math.random()}>{capetalizeFirstLetter(header)}</th>
+                );
+              })}
+              <th key={Math.random()}>Actions</th>
+            </tr>
+          </thead>
+          <tbody className="table-body">
+            {currentItems
+              .filter((row) => {
+                let res = false;
+                let vals = Object.values(row);
+                if (isURL(vals[length - 1])) vals.pop();
+                for (const val of vals) {
+                  res = val
+                    .toString()
+                    .toLowerCase()
+                    .includes(searchedValue.toString().toLowerCase());
+                  if (res) break;
+                }
+                return res;
+              })
+              .map((item) => {
+                return (
+                  <tr key={item.id}>
+                    {headers.map((header) => {
+                      return header === "image" ? (
+                        <td>
+                          <img className="product_image" src={item.image} />
+                        </td>
+                      ) : (
+                        <td key={Math.random()}>{item[header]}</td>
+                      );
+                    })}
                     <td>
-                      <img className="product_image" src={item.image} />
+                      <i className="fa-solid fa-trash"></i>
+                      <i
+                        className="fa-solid fa-pen-to-square"
+                        onClick={() => editProduct(item.code)}
+                      ></i>
                     </td>
-                  ) : (
-                    <td key={Math.random()}>{item[header]}</td>
-                  );
-                })}
-                <td>
-                  <i className="fa-solid fa-trash"></i>
-                  <i
-                    className="fa-solid fa-pen-to-square"
-                    onClick={() => editProduct(item.code)}
-                  ></i>
-                </td>
-              </tr>
-            );
-          })}
-      </tbody>
-    </table>
+                  </tr>
+                );
+              })}
+          </tbody>
+        </table>
+        </div>
+      <Pagination
+        currentPage={currentPage}
+        pageNumbers={pageNumbers}
+        setCurrentPage={setCurrentPage}
+      />
+    </div>
   );
 };
 export default FilterableTable;
