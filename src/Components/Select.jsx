0f99ac7a192355a4d195165c1ae3081adb2ba1b2
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-25 20:34:14 +0300

    implement reusable formik select list

---
+++ src/Components/Select.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+import TextError from "./TextError.jsx";
+import "./form.css";
+
+function Select({ label, name, options, ...rest }) {
+  return (
+    <div className="main-input-container">
+      <div className="input-container">
+        <label htmlFor={name} className="fieldslabel">
+          {label}
+        </label>
+        <Field
+          as="select"
+          id={name}
+          name={name}
+          {...rest}
+          className="select-field"
+        >
+          <option value="" className="select-option">
+          </option>
+          {options.map((option) => {
+            return (
+              <option
+                key={option.value}
+                value={option.value}
+                className="select-option"
+              >
+                {option.name}
+              </option>
+            );
+          })}
+        </Field>
+      </div>
+      <ErrorMessage component={TextError} name={name} />
+    </div>
+  );
+}
+
+export default Select;
