d35d7140b70dd16358c0be532567afd6f819e7e7
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-25 19:02:32 +0300

    implement modal component

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+import "./modal.css";
+
+const Modal = ({ children }) => {
+  const elRef = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+    return () => modalRoot.removeChild(elRef.current);
+  }, []);
+
+  return createPortal(
+    <div className="main-modal-div">{children}</div>,
+    elRef.current
+  );
+};
+
+export default Modal;
