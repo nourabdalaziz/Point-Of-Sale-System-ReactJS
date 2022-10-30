c814bcae082afe498ea8ec3978ac4a5a6f1a01c4
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-30 20:51:13 +0300

    setting products data context

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,43 @@ import Categories from "./Pages/Categories.jsx";
 import POS from "./Pages/POS.jsx";
 import Products from "./Pages/Products.jsx";
 import Navbar from "./Components/Navbar.jsx";
+import ProductsDataContext from "./Contexts/ProductsDataContext.jsx";
+import { useState, useEffect } from "react";
+import useFetch from "./CustomHooks/useFetch.jsx";
+
 const App = () => {
+  const [needToRefreshData, setNeedToRefreshData] = useState(false);
+  const [context, setContext] = useState([]);
+
+  const [products, isLoading] = useFetch(
+    "http://localhost:5000/products",
+    needToRefreshData
+  );
+
+  useEffect(() => {
+    products && setContext(products);
+  }, [products]);
+
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<POS />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/pos" element={<POS />} />
-      </Routes>
-    </BrowserRouter>
+    <ProductsDataContext.Provider
+      value={{
+        context,
+        setContext,
+        needToRefreshData,
+        setNeedToRefreshData,
+        isLoading,
+      }}
+    >
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<POS />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/pos" element={<POS />} />
+        </Routes>
+      </BrowserRouter>
+    </ProductsDataContext.Provider>
   );
 };
-
 export default App;
