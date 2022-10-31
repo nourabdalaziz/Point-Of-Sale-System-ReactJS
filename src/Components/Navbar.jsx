5622ddf52a7501caeb2df4a1c0c29805ee716799
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-31 18:57:48 +0300

    add logo image in the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,15 +4,16 @@ import "./navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <Link to="/" className="siteName">
-        POS
+      <Link to="/" className="siteName" style={{ fontWeight: "1000" }}>
+        <img src="https://img.icons8.com/external-smashingstocks-flat-smashing-stocks/50/000000/external-Pos-smooth-conceptual-smashingstocks-flat-smashing-stocks.png" />
+        <div> Point Of Sale</div>
       </Link>
       <ul className="navbar-titles">
-        <Link to="/categories" className="navLink">
-          Categoris
-        </Link>
         <Link to="/pos" className="navLink">
           POS
+        </Link>
+        <Link to="/categories" className="navLink">
+          Categoris
         </Link>
         <Link to="/products" className="navLink">
           Products
