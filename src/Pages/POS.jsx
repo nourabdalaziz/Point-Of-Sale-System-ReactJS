cf39a41c4a3a9cdef6964d3d59116186d2d035f6
Nour Abd Al-Aziz <noorimadaziz@gmail.com>
2022-10-31 11:14:51 +0300

    refactor pos page to use cart component

--- src/Pages/POS.jsx
+++ src/Pages/POS.jsx
@@ -3,6 +3,7 @@ import ProductsDataContext from "../Contexts/ProductsDataContext.jsx";
 import LoadingSpinner from "../Components/LoadingSpinner.jsx";
 import FilterableGrid from "../Components/FilterableGrid.jsx";
 import useFetch from "../CustomHooks/useFetch.jsx";
+import Cart from "../Components/Cart.jsx";
 
 const POS = () => {
   const { context, isLoading } = useContext(ProductsDataContext);
@@ -110,7 +111,6 @@ const POS = () => {
               />{" "}
               <div className="pos-categories-btns">
                 <button onClick={() => setSearchedValue("")}>All</button>
-
                 {categories &&
                   categories.map((category) => {
                     return (
@@ -127,75 +127,14 @@ const POS = () => {
                   })}
               </div>
             </div>
-
-            <div className="cart">
-              <h2>Cart</h2>
-              <div className="cart-items-list">
-                {cart.map((item) => {
-                  return (
-                    <div key={Math.random()} className="itemInCart">
-                      <i
-                        className="fa fa-times-circle fa-2x"
-                        aria-hidden="true"
-                        onClick={() => deleteFromCart(item)}
-                      ></i>
-                      <img
-                        src={item.image}
-                        style={{ height: "50px", width: "50px" }}
-                      />
-                      <div className="itemInCart-nameAndPrice">
-                        <div>{item.name}</div>
-                        <div>{item.price} $</div>
-                      </div>
-                      <div className="itemInCart-quantity-and-buttons">
-                        <i
-                          className="fa fa-minus-circle fa-2x"
-                          onClick={() => editQtty(item, "minus")}
-                          aria-hidden="true"
-                        ></i>
-
-                        <div className="itemInCart-quantity">
-                          {item.quantity}
-                        </div>
-
-                        <i
-                          className="fa fa-plus-circle fa-2x"
-                          onClick={() => editQtty(item, "plus")}
-                          aria-hidden="true"
-                        ></i>
-                      </div>
-                      <div className="itemInCart-totalPerProduct">
-                        {item.totalPerProduct} $
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-              <div className="cart-tax-disc-total">
-                <div className="cart-input-container">
-                  <label htmlFor="tax-input">Tax:</label>{" "}
-                  <input
-                    id="tax-input"
-                    placeholder="%"
-                    className="cart-input-field"
-                    type="text"
-                    onChange={(e) => setTax(e.target.value)}
-                  />
-                </div>
-                <div className="cart-input-container">
-                  <label htmlFor="discount-input">Discount :</label>{" "}
-                  <input
-                    id="discount-input"
-                    placeholder="%"
-                    className="cart-input-field"
-                    type="text"
-                    onChange={(e) => setDiscount(e.target.value)}
-                  />{" "}
-                </div>
-                <div className="cart-total-sum">
-                 Total : {total} $</div>
-              </div>
-            </div>
+            <Cart
+              dataInCart={cart}
+              deleteFromCart={deleteFromCart}
+              editQtty={editQtty}
+              setTax={setTax}
+              setDiscount={setDiscount}
+              total={total}
+            />
           </div>
         </>
       )}
